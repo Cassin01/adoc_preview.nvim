d085e83b1a116569ab562814d35c7a6958fbee00
Cassin01 <koyojb@gmail.com>
2022-12-12 07:06:02 +0900

    update: denops/adoc_preview/server.ts

--- denops/adoc_preview/server.ts
+++ denops/adoc_preview/server.ts
@@ -4,7 +4,7 @@ import { Server } from "https://deno.land/x/socket_io@0.1.1/mod.ts";
 
 async function getHtml(): Promise<any> {
   const html = await readTextFile(
-    "./routes/index.html",
+    "./routes/index.html"
     // {
     //   encoding: "utf8",
     // },
