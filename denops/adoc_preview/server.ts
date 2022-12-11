fcc1c2c631b3bd9cb799f579b8957efa609470f6
Cassin01 <koyojb@gmail.com>
2022-12-12 07:08:59 +0900

    update: denops/adoc_preview/server.ts

--- denops/adoc_preview/server.ts
+++ denops/adoc_preview/server.ts
@@ -5,13 +5,14 @@ import { Server } from "https://deno.land/x/socket_io@0.1.1/mod.ts";
 async function getHtml(): Promise<any> {
   const html = await readTextFile(
     "./routes/index.html"
+  );
     // {
     //   encoding: "utf8",
     // },
-  ).catch((err) => {
-    console.log(err);
-    return "failed to read a file";
-  });
+  // ).catch((err) => {
+  //   console.log(err);
+  //   return "failed to read a file";
+  // });
 
   return { html };
 }
