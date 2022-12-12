15b3cda5825f2a0bfff43c9063df49f15d163927
Cassin01 <koyojb@gmail.com>
2022-12-12 16:50:21 +0900

    fix: echo path

--- denops/adoc_preview/main.ts
+++ denops/adoc_preview/main.ts
@@ -1,7 +1,7 @@
 import Server from "./server.ts";
 
 // import { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts";
-import { Denops } "./deps.ts";
+import { Denops } from "./deps.ts";
 
 export async function main(denops: Denops): Promise<void> {
     denops.dispatcher = {
